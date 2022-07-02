8facd69e38734eac019550d8d73233bf033d8de8
CrisNeron2001 <cris.riffo@duocuc.cl>
2022-07-02 09:38:29 -0400

    commit

--- SmartPointOfSales/settings.py
+++ SmartPointOfSales/settings.py
@@ -73,13 +73,13 @@ TEMPLATES = [
         },
     },
 ]
-"""
+
 REST_FRAMEWORK ={
-    'DEFAULT_AUTHENTICATION_CLASES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
-"""
+
 WSGI_APPLICATION = 'SmartPointOfSales.wsgi.application'
 
 
